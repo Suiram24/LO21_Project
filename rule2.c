a3dc787084a1912bc4f8c2d349a0ea6f965f8581
Louis-Esteban <65354752+Louis-Esteban@users.noreply.github.com>
2020-12-29 17:22:13 +0100

    Update rule2.c

--- rule2.c
+++ rule2.c
@@ -5,51 +5,53 @@ typedef struct rule {
 };
 
 struct rule* add_fact(fact, *rule){
-    if (rule=NULL){
-        return rule;
+    if (rule.next==NULL){
+        return 0;
     } else {
-        struct rule* rule_transit=rule;
-        while (rule_transit->next !=NULL){
-            rule_transit = rule_transit->next;
-            rule = rule->next;
+        struct rule* rule_transit;
+        while (rule.next !=NULL){
+            rule = rule.next;
         }
-        rule.fact = fact;
-        rule.ccl=0;
-        rule = rule -> next;
-        rule.fact=rule_transit.fact;
-        rule.ccl=1;
+        if (rule.ccl==1){
+            rule_transit.next=*rule;
+            rule.next=*fact;
+            rule=rule.next;
+            rule.next=*rule_transit;
+        } else {
+            rule.next=*fact;
+        }
+        return 1;
     }
     return rule;
 }
 
 int test_fact(fact, *rule){
-    while (rule->next!=NULL){
-        if (rule.fact==fact){
-            break;
+    while (rule.next!=NULL){
+        if (strcmp(rule.fact,fact)==0){
             return 1;
         }
         rule=rule->next;
     }
-    if (rule->next!=NULL){
+    if (rule.next!=NULL){
         return 0;
     }
 }
 
 int remove_fact(fact, *rule){
     struct rule* rule_trst=rule;
-    if (rule==NULL){
-        return rule;
+    if (rule.next==NULL){
+        return 0;
     } else {
-        while (rule_trst->next!=NULL){
-          if (rule_trst.fact==fact){
+        while (rule.next!=NULL){
+          if (strcmp(rule.fact,fact)==0){
               break;
           }
           rule_trst=rule_trst->next;
           rule=rule->next;
         }
         rule_trst=rule_trst->next;
-        if (rule_trst->next!=NULL){
-            return 0;
+        if (rule.next!=NULL){
+            return 1;
         }
         while (rule_trst->next!=NULL){
         rule=rule_trst;
@@ -60,7 +62,7 @@ int remove_fact(fact, *rule){
 }
 
 *char rule_head_fact(*rule){
-    if (rule==NULL){
+    if (rule.next==NULL){
         return rule;
     } else {
       return rule.fact;
